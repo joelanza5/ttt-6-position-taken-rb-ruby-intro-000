1b9f1dfeaf58b9c2553b1ee5711205c1a83696f1
JOSEPH LANZA <joelanza5@gmail.com>
2020-03-25 16:45:00 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,11 @@
 def position_taken?(board, index)
   if board[index] == " "
     false
+  elsif board[index] == ""
+    false
+  elsif board[index] == nil
+    false
+  elsif board[index] == "X" || "O"
+    true
   end
 end
